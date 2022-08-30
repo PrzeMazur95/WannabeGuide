620089872d62c489e7a2106e3dad2d3a54c79d85
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-30 18:59:46 +0200

    Add fillable properties to Topic model

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category',
+        'description',
+        'status',
+        'user_id'
+    ];
 }
