85ea70ce002540b29fc9847e3509ce8dc8192ce0
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-30 22:44:40 +0200

    Add feature test to see if we could get all topics

--- tests/Feature/Api/TopicControllerTest.php
+++ tests/Feature/Api/TopicControllerTest.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Topic;
 
 class TopicControllerTest extends TestCase
 {
@@ -18,5 +21,35 @@ class TopicControllerTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+
+    }
+
+    /**
+     * If api returns all topics
+     *
+     * @return void
+     * @test
+     */
+    public function if_we_could_see_all_topics(): void
+    {
+        $topic = Topic::factory()->create();
+
+        $response = $this->getJson('api/topics');
+        // dd($response);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('topics', [
+            'name'=>$topic->name
+        ]);
+        $response->assertJsonStructure([
+            0 => [
+                "id",
+                "name",
+                "category",
+                "description",
+                "status",
+                "user_id",
+            ]
+        ]);
     }
 }
