fd10770698b14eec95ad9682d66c07c1bd7fb0e6
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-30 19:23:29 +0200

    Add api route to get all topics

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TopicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(TopicController::class)->group(function () {
+    //route to see all existing topics
+    Route::get('topics', 'index');
 });
