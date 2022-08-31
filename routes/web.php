2b50d0c610107cb9349fe3ccdc087aa1af5d00c8
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-31 19:18:18 +0200

    Reorganize web routes, to only one time declare middleware auth

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
