4c20d8ffea0ead0572f7741f6c0dfb270915b947
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-01 22:07:16 +0200

    Add all topics link to main navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('topics')" :active="request()->routeIs('topics')">
+                        {{ __('All Topics') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
