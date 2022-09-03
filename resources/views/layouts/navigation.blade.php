a18cf054f8080af998f2275b2b7d1a14afea9344
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-03 19:29:52 +0200

    Add link in navigation to go to add new topic

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('All Topics') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('topics.create')" :active="request()->routeIs('topics.create')">
+                        {{ __('Add new!') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -75,6 +80,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('topics.all')" :active="request()->routeIs('topics.all')">
                 {{ __('Topics') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('topics.create')" :active="request()->routeIs('topics.create')">
+                {{ __('Add new!') }}
             </x-responsive-nav-link>
         </div>
 
