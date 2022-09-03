d194eca49c20fe63f2bbec8bcac694845efb94d9
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-03 18:42:31 +0200

    Change names of routes to proper refactored name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('topics')" :active="request()->routeIs('topics')">
+                    <x-nav-link :href="route('topics.all')" :active="request()->routeIs('topics.all')">
                         {{ __('All Topics') }}
                     </x-nav-link>
                 </div>
@@ -73,7 +73,7 @@
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('topics')" :active="request()->routeIs('topics')">
+            <x-responsive-nav-link :href="route('topics.all')" :active="request()->routeIs('topics.all')">
                 {{ __('Topics') }}
             </x-responsive-nav-link>
         </div>
