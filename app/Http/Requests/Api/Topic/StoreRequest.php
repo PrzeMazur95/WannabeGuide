d35f98f5ad9e0e6deac0650ea3a0e705ffd317bf
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-05 19:38:52 +0200

    Fix bug in request file typo

--- app/Http/Requests/Api/Topic/StoreRequest.php
+++ app/Http/Requests/Api/Topic/StoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Api\Topic;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRequest.php extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
