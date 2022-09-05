d758cf9a46c130483431021b7185c3abbbb78e1b
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-05 23:14:47 +0200

    Add store request to api store method call

--- app/Http/Requests/Api/Topic/StoreRequest.php
+++ app/Http/Requests/Api/Topic/StoreRequest.php
@@ -13,7 +13,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'string', 'unique:topics'],
+            'description' => ['required', 'string'],
+            'user_id' => ['required', 'int']
         ];
     }
 }
