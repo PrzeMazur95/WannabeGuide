9c26306843bd693b3ab4c66d17f3497b34c11c1a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-06 23:29:12 +0200

    Dispaly every topic block as an url

--- resources/views/Topic/all_topics.blade.php
+++ resources/views/Topic/all_topics.blade.php
@@ -10,14 +10,16 @@
         </div>
     @endif
     @foreach($topics as $topic)
-    <div class="py-2">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    {{$topic->name}}
+     <div class="py-2">
+        <a href="{{ url('topic/'.$topic->id) }}">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        {{$topic->name}}
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
     @endforeach
 </x-app-layout>
