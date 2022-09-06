fff17731a94e94d784abb1547c1e42205454327c
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-06 23:13:15 +0200

    Add annotations to web routes

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::middleware('auth')->group(function(){
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 
     Route::controller(TopicController::class)->group(function () { 
+        //route to see all topics
         Route::get('/topics','index')->name('topics.all');
+        //route to show form of creating new topic
         Route::get('/topics/create', 'create')->name('topics.create');
+        //route to store new topic
         Route::post('/topics/create', 'store')->name('topics.store');
     });
 });
