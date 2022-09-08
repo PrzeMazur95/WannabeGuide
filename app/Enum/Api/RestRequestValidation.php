4dcc3a922d68d1457cad994b52bc5883df364f5a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-08 21:38:22 +0200

    Add to rest request validation enum category case

--- app/Enum/Api/RestRequestValidation.php
+++ app/Enum/Api/RestRequestValidation.php
@@ -9,9 +9,11 @@ enum RestRequestValidation: string
     case NAME_IS_REQUIRED = 'Name field is required';
     case DESCRIPTION_IS_REQUIRED = 'Description field is required';
     case USER_ID_IS_REQUIRED = 'User id is required';
+    case CATEGORY_ID_IS_REQUIRED = 'Category id is required';
 
     case NAME_FIELD_HAS_TO_BE_A_STRING = 'Name field has to be a string';
     case NAME_FIELD_HAS_TO_BE_UNIQUE = 'There is a topic with the same name';
     case DESCRIPTION_HAS_TO_BE_A_STRING = 'Description has to be a string';
     case USER_ID_HAS_TO_BE_AN_INT = 'User id has to be an integer';
+    case CATEGORY_ID_HAS_TO_BE_AN_INT = 'Category id has to be an integer';
 }
