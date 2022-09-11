e5d1b7038d434063839fc3c8c1efabd006335f01
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-11 22:28:40 +0200

    Add new enum

--- app/Enum/Api/RestResponses.php
+++ app/Enum/Api/RestResponses.php
@@ -7,4 +7,6 @@ namespace App\Enum\Api;
 enum RestResponses: string
 {
     case NEW_TOPIC_HAS_BEEN_ADDED = 'Your topic has been succesfully added!';
+
+    case ERROR_ADD_NEW_TOPIC = 'Something went wrong when adding new topic, please try one more time, or contact with admin';
 }
