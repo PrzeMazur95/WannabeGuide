54cdcc106de074013d286803b0368146a30970f2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-12 21:18:52 +0200

    Add new enum message for api call all posts error

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -7,4 +7,5 @@ namespace App\Enum\Api;
 enum LoggerMessages: string
 {
     case ERROR_NEW_TOPIC_ADD = 'Something went wrong when adding new topic through API!';
+    case ERROR_GET_ALL_POSTS = 'Something went wrong when trying to get all posts through API!';
 }
