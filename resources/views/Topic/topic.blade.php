f9976866df6e8a27da9d697179c393ba48e1127c
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-16 21:49:58 +0200

    Add two button on specific topic page to edit end delete

--- resources/views/Topic/topic.blade.php
+++ resources/views/Topic/topic.blade.php
@@ -1,15 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
     <div class="py-2 text-center">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight py-2">
             {{$topic->name}}
         </h2>
-        <x-button_white class="ml-3">
-            {{ __('Delete') }}
-        </x-button>
-        <x-button class="ml-3">
-            {{ __('Delete') }}
-        </x-button>
+        <form class="inline-flex" method="POST" action="{{ route('topics.delete', ['topic'=>$topic->id]) }}">
+            <input type="hidden" name="_method" value="PATCH">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <x-button_white class="ml-3">
+                {{ __('Edit') }}
+            </x-button>
+        </form>
+        <form class="inline-flex" method="POST" action="{{ route('topics.edit', ['topic'=>$topic->id]) }}">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <x-button class="ml-3">
+                {{ __('Delete') }}
+            </x-button>
+        </form>
     </div>
     </x-slot>
      <div class="py-2">
