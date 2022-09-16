473c0ac5269a26c8959063a8829dad827a6abd54
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-16 21:16:07 +0200

    Added more class to button to be more familiar to gray one

--- resources/views/components/button_white.blade.php
+++ resources/views/components/button_white.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex bg-white uppercase hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 text-xs rounded shadow']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex bg-white uppercase tracking-widest hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-800 rounded-md text-xs rounded shadow ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
