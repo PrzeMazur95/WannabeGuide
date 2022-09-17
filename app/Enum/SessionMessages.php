1a68d381a0d4d631cc36885ee295e7030ff63893
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-17 17:27:39 +0200

    Fix typo in enum file name

---
+++ app/Enum/SessionMessages.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum SessionMessages: string
+{
+    case TOPIC_ADDED = 'Topic has been succesfully added!';
+    case TOPIC_DELETED = 'Topic has been succesfully deleted!!';
+}
