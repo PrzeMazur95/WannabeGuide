4c33290815176ac47e29977d5bde0877140b6d06
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-22 22:07:42 +0200

    Add new enum msgs to request responses

--- app/Http/Requests/Api/Topic/ShowRequest.php
+++ app/Http/Requests/Api/Topic/ShowRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\Topic;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Enum\Api\RestRequestValidation;
 
 class ShowRequest extends FormRequest
 {
@@ -24,7 +25,20 @@ class ShowRequest extends FormRequest
     public function rules()
     {
         return [
-            'id' => ['required', 'int']
+            "id" => "required|int"
+        ];
+    }
+
+    /**
+     * Describes specific messages which are declared in RestRequestValidation enum file
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'id.required' => RestRequestValidation::TOPIC_ID_IS_REQUIRED->value,
+            'id.int' => RestRequestValidation::TOPIC_ID_HAS_TO_BE_AN_INT->value
         ];
     }
 }
