cccbb0ea7e654f6a05652f329d177ed18fabf5c8
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-23 18:44:02 +0200

    Add new enum msg to log db query was failed

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -8,4 +8,5 @@ enum LoggerMessages: string
 {
     case ERROR_NEW_TOPIC_ADD = 'Something went wrong when adding new topic through API!';
     case ERROR_GET_ALL_TOPICS = 'Something went wrong when trying to get all topics through API!';
+    case ERROR_SHOW_SPECIFIC_TOPIC = 'Something went wrong when trying to show speciifc topic through API!';
 }
