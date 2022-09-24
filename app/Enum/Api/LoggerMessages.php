8ea09f8d209a168c64b5bb44b473c7f3f62ceb49
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-24 11:58:32 +0200

    Add logger enum msg when smth went wrong with topi update

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -9,4 +9,5 @@ enum LoggerMessages: string
     case ERROR_NEW_TOPIC_ADD = 'Something went wrong when adding new topic through API!';
     case ERROR_GET_ALL_TOPICS = 'Something went wrong when trying to get all topics through API!';
     case ERROR_SHOW_SPECIFIC_TOPIC = 'Something went wrong when trying to show speciifc topic through API!';
+    case ERROR_UPDATE_TOPIC = 'Something went wrong when trying to update this topic through API!';
 }
