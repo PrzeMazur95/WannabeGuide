d0c786bd9a5e4319a5d143f972876c66e692d97a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-28 16:57:32 +0200

    Add enum msg when there is some error getting user form db in api

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -10,4 +10,5 @@ enum LoggerMessages: string
     case ERROR_GET_ALL_TOPICS = 'Something went wrong when trying to get all topics through API!';
     case ERROR_SHOW_SPECIFIC_TOPIC = 'Something went wrong when trying to show speciifc topic through API!';
     case ERROR_UPDATE_TOPIC = 'Something went wrong when trying to update this topic through API!';
+    case ERROR_GET_USER_FROM_DB = 'Something went wrong when trying to get user from db through API!';
 }
