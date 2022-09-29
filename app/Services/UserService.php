a938a530b18670927d5eabf9a96154187066331c
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-09-29 18:13:01 +0200

    Add method do user service to check if user is an owner of typed topic

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,15 +5,18 @@ namespace App\Services;
 use App\Models\User;
 use App\Enum\Api\LoggerMessages;
 use Illuminate\Support\Facades\Log;
+use App\Models\Topic;
 
 class UserService {
 
     public function __construct(
         private User $user,
+        private Topic $topic,
+        private Log $logger
     ){
     }
 
-    public function checkIfExists(int $user_id)
+    public function checkIfExists(User $user_id)
     {
         try{
             if(!$this->user::find($user_id)){
@@ -28,5 +31,23 @@ class UserService {
             return false;
         }
 
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param int $user_id
+     * @param int $topic
+     * @return void
+     */
+    public function checkIfUserIsAnOwnerOfSpecificTopic(int $user_id, int $topic){
+
+        if($this->topic::find($topic)->user_id === $user_id){
+
+            return true;
+        } else {
+            
+            return false;
+        }
     }
 }
