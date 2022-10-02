26ca1745ced745f9b8ca5a1beba7b7db7c45994f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-02 09:10:46 +0200

    Add grid on dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,9 @@
             </div>
         </div>
     </div>
+    <div class="grid grid-rows-3 grid-flow-col gap-4">
+  <div class="row-span-3 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">01</div>
+  <div class="col-span-2 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">02</div>
+  <div class="row-span-2 bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-2 sm:px-6 lg:px-8 p-6 mx-2 ...">03</div>
+</div>
 </x-app-layout>
