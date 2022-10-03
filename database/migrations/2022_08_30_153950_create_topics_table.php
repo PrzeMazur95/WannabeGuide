bdf4f079650ce8c57cf417f65b878bc02d3ccfa8
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-03 18:35:06 +0200

    Refactor Topics migration to future Category feature

--- database/migrations/2022_08_30_153950_create_topics_table.php
+++ database/migrations/2022_08_30_153950_create_topics_table.php
@@ -16,9 +16,8 @@ return new class extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('category');
+            $table->integer('category');
             $table->longtext('description');
-            $table->integer('status')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
