8a44ea2d6466a8f76fdc3c80f4dc4c3fd8cf45e8
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-07 13:45:10 +0200

    Add select on topic page, to future select category

--- resources/views/Topic/new_topic.blade.php
+++ resources/views/Topic/new_topic.blade.php
@@ -25,7 +25,15 @@
                     <div class="relative z-0 mb-6 w-full group">
                         <textarea id="description" name="description" rows="4" class=" block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write something about it..." required>{{ old('description') }}</textarea>
                     </div>
-                    <x-button class="ml-3">
+                    <label for="categories" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select category</label>
+                        <select id="categories" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Choose a country</option>
+                            <option value="US">United States</option>
+                            <option value="CA">Canada</option>
+                            <option value="FR">France</option>
+                            <option value="DE">Germany</option>
+                        </select>
+                    <x-button class="ml-3 p-6">
                     {{ __('Submit') }}
                     </x-button>
                 </form>
