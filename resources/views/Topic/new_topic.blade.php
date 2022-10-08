772c7ef1d81da9d15f3ccb332648db0f954510f9
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-08 17:43:40 +0200

    Displays on new topic grid all cateogires as a select field

--- resources/views/Topic/new_topic.blade.php
+++ resources/views/Topic/new_topic.blade.php
@@ -27,10 +27,9 @@
                     </div>
                         <select id="category" name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected>Choose a category</option>
-                            <option value="US">United States</option>
-                            <option value="CA">Canada</option>
-                            <option value="FR">France</option>
-                            <option value="DE">Germany</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
                         </select>
                     <x-button class="mt-5">
                     {{ __('Submit') }}
