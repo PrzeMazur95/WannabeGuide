87686f206a8cfa8d200bda77cedcb779d2499122
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-11 18:28:34 +0200

    Some refactor with phpstan

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,28 @@ use App\Http\Controllers\TopicController;
 |
 */
 
-Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group( function () {
 
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 
-    Route::controller(TopicController::class)->group(function () { 
-         //route to save updated topic
-         Route::patch('/topics/{topic}/update', 'update')->name('topics.update');
-        //route to see all topics
-        Route::get('/topics','index')->name('topics.all');
-        //route to show form of creating new topic
-        Route::get('/topics/create', 'create')->name('topics.create');
-        //route to store new topic
-        Route::post('/topics/create', 'store')->name('topics.store');
-        //route to see specific topic
-        Route::get('/topics/{topic}', 'show')->name('topics.show');
-        //route to delete specific topic
-        Route::delete('/topics/{topic}', 'destroy')->name('topics.delete');
-        //route to edit specific topic
-        Route::patch('/topics/{topic}', 'edit')->name('topics.edit');
-    });
+    Route::controller(TopicController::class)->group(
+        function () { 
+            //route to save updated topic
+            Route::patch('/topics/{topic}/update', 'update')->name('topics.update');
+            //route to see all topics
+            Route::get('/topics', 'index')->name('topics.all');
+            //route to show form of creating new topic
+            Route::get('/topics/create', 'create')->name('topics.create');
+            //route to store new topic
+            Route::post('/topics/create', 'store')->name('topics.store');
+            //route to see specific topic
+            Route::get('/topics/{topic}', 'show')->name('topics.show');
+            //route to delete specific topic
+            Route::delete('/topics/{topic}', 'destroy')->name('topics.delete');
+            //route to edit specific topic
+            Route::patch('/topics/{topic}', 'edit')->name('topics.edit');
+        }
+    );
     Route::controller(CategoryController::class)->group(
         function () {
             Route::get('/category/create', 'create')->name('category.create');
