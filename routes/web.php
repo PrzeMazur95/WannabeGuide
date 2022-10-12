5eba07c68bcfc1f935adfaae3799a97a39c5f0b9
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-12 17:44:20 +0200

    Add category controller to web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopicController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
