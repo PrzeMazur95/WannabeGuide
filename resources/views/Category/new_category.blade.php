726731582da0f6a9b6382d57140934eff703a872
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-13 16:59:04 +0200

    Refactor new category view to esnd proper data to store method in controller

--- resources/views/Category/new_category.blade.php
+++ resources/views/Category/new_category.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <form method="POST" action="{{ route('topics.store') }}">
+                <form method="POST" action="{{ route('category.store') }}">
                     @csrf
                     @if ($errors->any())
                         @foreach ($errors->all() as $error)
@@ -20,11 +20,9 @@
                     @endif
                     <div class="relative z-0 mb-6 w-full group">
                         <input type="text" name="name" id="name" value="{{ old('name') }}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                        <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Topic tittle</label>
+                        <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Category name</label>
                     </div>
-                    <div class="relative z-0 mb-6 w-full group">
-                        <textarea id="description" name="description" rows="4" class=" block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write something about it..." required>{{ old('description') }}</textarea>
-                    </div>
+                    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
                     <x-button class="mt-5">
                     {{ __('Submit') }}
                     </x-button>
