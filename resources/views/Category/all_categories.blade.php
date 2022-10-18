8661d6d59fcf44b949a7e916eb7a7b36029c2272
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-18 23:27:48 +0200

    Adjust view to show informations about given categories

--- resources/views/Category/all_categories.blade.php
+++ resources/views/Category/all_categories.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            List of all topics
+            List of all categories
         </h2>
     </x-slot>
-    @if(Session::has('TOPIC_ADDED'))
+    @if(Session::has('CATEGORY_ADDED'))
         <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
-            <span class="font-medium">{{ Session::get('TOPIC_ADDED')}}</span>
+            <span class="font-medium">{{ Session::get('CATEGORY_ADDED')}}</span>
         </div>
-    @elseif(Session::has('TOPIC_DELETED'))
+    @elseif(Session::has('CATEGORY_DELETED'))
         <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
-            <span class="font-medium">{{ Session::get('TOPIC_DELETED')}}</span>
+            <span class="font-medium">{{ Session::get('CATEGORY_DELETED')}}</span>
         </div>
-     @elseif(Session::has('TOPIC_UPDATED'))
+     @elseif(Session::has('CATEGORY_UPDATED'))
         <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
-             <span class="font-medium">{{ Session::get('TOPIC_UPDATED')}}</span>
+             <span class="font-medium">{{ Session::get('CATEGORY_UPDATED')}}</span>
         </div>
     @endif
-    @foreach($topics as $topic)
+    @foreach($categories as $category)
      <div class="py-2">
-        <a href="{{ url('topics/'.$topic->id) }}">
+        <a href="{{ url('category/'.$category->id) }}">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        {{$topic->name}}
+                        {{$category->name}}
                     </div>
                 </div>
             </div>
