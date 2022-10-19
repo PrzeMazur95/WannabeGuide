112d5bf8e89c3ee56c1b16897e0609cfa307f19f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-19 20:16:24 +0200

    Add new enum msg when category is created

--- app/Enum/SessionMessages.php
+++ app/Enum/SessionMessages.php
@@ -9,4 +9,6 @@ enum SessionMessages: string
     case TOPIC_ADDED = 'Topic has been succesfully added!';
     case TOPIC_DELETED = 'Topic has been succesfully deleted!!';
     case TOPIC_UPDATED = 'Topic has been succesfully updated !';
+
+    case CATEGORY_ADDED = 'Category has been succesfully added!';
 }
