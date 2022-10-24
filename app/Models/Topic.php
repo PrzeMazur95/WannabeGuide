a5eeae79b68595e3b0288272775729b4d7ec2676
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-24 22:31:21 +0200

    Add relationship between topic and its author

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class Topic extends Model
 {
@@ -16,4 +18,14 @@ class Topic extends Model
         'status',
         'user_id'
     ];
+
+    /**
+     * Relation between topic and User
+     *
+     * @return relationship
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
