f55236a5c4aaaaf456c3fa68c4505c789ee7f14a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-26 22:18:20 +0200

    Add test if we could see db data on all_categories page

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -37,4 +37,21 @@ class CategoryControllerTest extends TestCase
         $response->assertViewIs('Category.all_categories');
         $response->assertStatus(200);
     }
+    
+    /**
+     * Test if we could get categories from db and render it to the user
+     *
+     * @test
+     * @return void
+     */
+    public function if_we_could_see_categories_data_on_its_main_page()
+    {
+
+        $categories =$this->category::factory()->count(5)->create();
+        
+        $view = $this->view('Category/all_categories', ['categories'=>$categories]);
+
+        $view->assertSee($categories[1]->name);
+
+    }
 }
