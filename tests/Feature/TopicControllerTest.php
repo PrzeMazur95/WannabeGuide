0fb9ac3ef51e2289cdc04b0aa02fb3758f0a2dbf
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-28 22:24:27 +0200

    Delete unnecesary topic render main page test

--- tests/Feature/TopicControllerTest.php
+++ tests/Feature/TopicControllerTest.php
@@ -20,26 +20,6 @@ class TopicControllerTest extends TestCase
     }
 
     /**
-     * Test if we could render topics page properly
-     *
-     * @return void
-     * @test
-     */
-    public function if_we_could_render_topics_page_properly()
-    {
-        // $topic = $this->topic->factory()->create();
-        // $AllTopics = $this->topic::all();
-        // $view = $this->view('Topic/AllTopics', ['topics' => $AllTopics]);
-        // $view->assertSee('sampleTopic');
-
-        $response = $this->get('/topics');
-
-        $response->assertStatus(200);
-
-
-    }
-
-    /**
      * Test if we could render db data on topics page properly
      *
      * @return void
