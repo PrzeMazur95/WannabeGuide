0b00065fe9d7af123a895beefe414353e16d600e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-10-29 08:19:48 +0200

    Change foreach loop to forelse in category page

--- resources/views/Category/category.blade.php
+++ resources/views/Category/category.blade.php
@@ -4,7 +4,7 @@
             List of all topics belongs to this category
         </h2>
     </x-slot>
-    @foreach($category->topics as $topic)
+    @forelse($category->topics as $topic)
      <div class="py-2">
         <a href="{{ url('category/'.$category->id) }}">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -16,5 +16,15 @@
             </div>
         </a>
     </div>
-    @endforeach
+    @empty
+    <div class="py-2">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-red-100 border-b border-gray-200">
+                    <p>There are no topics in this category</p>
+                </div>
+            </div>
+         </div>
+    </div>
+    @endforelse
 </x-app-layout>
