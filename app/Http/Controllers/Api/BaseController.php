8bb45b41f93784aee5b3f1314aa29e30afd67470
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-09 17:53:36 +0100

    Use logger in base controller

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Enum\Api\LoggerMessages;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BaseController extends Controller
 {
+    private $logger;
 
-    public function catch($e,$loggerMsg, $restResponse, $responseCode){
+    public function __construct($logger)
+    {
+        $this->logger = $logger;
+    }
 
-        $this->logger::error(LoggerMessages::ERROR_GET_ALL_TOPICS->value, ['error' => $e->getMessage()]);
+    public function catch($e,$loggerMsg, $restResponse, $responseCode)
+    {
+        // dd($this->logger);
+        // $this->logger::error(LoggerMessages::ERROR_GET_ALL_TOPICS->value, ['error' => $e->getMessage()]);
+        $this->logger::error($loggerMsg, ['error' => $e->getMessage()]);
+        dd('try logger');
 
         return response()->json(RestResponses::ERROR_GET_ALL_TOPICS, $this->responseCode::HTTP_INTERNAL_SERVER_ERROR);
     }
