19f502647358606c6ff86f04e992f2b6fd105fac
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-09 07:06:10 +0100

    Add first step to outsource catch block to base controller

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+
+    public function catch($e,$loggerMsg, $restResponse, $responseCode){
+
+        $this->logger::error(LoggerMessages::ERROR_GET_ALL_TOPICS->value, ['error' => $e->getMessage()]);
+
+        return response()->json(RestResponses::ERROR_GET_ALL_TOPICS, $this->responseCode::HTTP_INTERNAL_SERVER_ERROR);
+    }
 }
