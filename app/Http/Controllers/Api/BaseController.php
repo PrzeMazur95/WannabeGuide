b323ca4f5eb5ff309ae74451669c6838779f92bb
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-10 17:11:47 +0100

    Add as an use rest responses to api base controller

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Enum\Api\LoggerMessages;
+use App\Enum\Api\RestResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
