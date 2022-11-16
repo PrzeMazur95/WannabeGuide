891d8010d0a29a8e9ccc32e04c7c7f92ac588139
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-16 17:28:45 +0100

    Add api test if we could create a new category

--- tests/Feature/Api/CategoryControllerTest.php
+++ tests/Feature/Api/CategoryControllerTest.php
@@ -10,10 +10,28 @@ use App\Models\User;
 class CategoryControllerTest extends TestCase
 {
     /**
-     * Test if we could get all categories
+     * Test if we could add new category
      *
      * @return void
      * @test
+     */
+    public function if_we_could_add_new_category()
+    {
+        $response = $this->postJson(
+            'api/category', [
+                'name'=>'test',
+                'user_id'=>User::factory()->create()->id
+            ]
+        );
+
+        $response->assertCreated();
+    }
+
+    /**
+     * Test if we could get all categories
+     *
+     * @return void
+     * 
      */
     public function if_we_could_return_all_categories()
     {
