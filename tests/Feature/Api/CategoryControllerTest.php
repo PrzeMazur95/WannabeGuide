85ce3dc09176a4e42a4dd6d7a69a456b3b07b1e9
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-16 17:51:24 +0100

    Add test if we could get all categories through api call

--- tests/Feature/Api/CategoryControllerTest.php
+++ tests/Feature/Api/CategoryControllerTest.php
@@ -31,14 +31,22 @@ class CategoryControllerTest extends TestCase
      * Test if we could get all categories
      *
      * @return void
-     * 
+     * @test
      */
     public function if_we_could_return_all_categories()
     {
-        //to be contiuned after adding route to create a category via API
         $response = $this->getJson('api/category');
 
         $response->assertStatus(200)
-            ->hasAll(['id','name','user_id','created_at','updated_ut']);
+            ->assertJsonStructure( [ 
+                0 => [
+                "id",
+                "name",
+                "user_id",
+                "created_at",
+                "updated_at"
+                ]
+                ]
+            );
     }
 }
