b1df65a4ba5c9d1457b91a857cbe197bf8f62273
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-16 17:18:31 +0100

    Add user clas to api categorycontroller test

--- tests/Feature/Api/CategoryControllerTest.php
+++ tests/Feature/Api/CategoryControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class CategoryControllerTest extends TestCase
 {
