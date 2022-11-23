1abf888583bcba1737c86fc495b125344b12df6f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-23 17:34:47 +0100

    Fill required properties in api delete category request

--- app/Http/Requests/Api/Category/DeleteRequest.php
+++ app/Http/Requests/Api/Category/DeleteRequest.php
@@ -13,7 +13,7 @@ class DeleteRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class DeleteRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'id'=>'required|int',
+            'user_id'=>'required|int'
         ];
     }
 }
