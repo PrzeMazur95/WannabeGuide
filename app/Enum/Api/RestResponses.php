c6292f925f3ec9ff81cd212d37f59c7d7bdd783d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-25 21:31:55 +0100

    Add enum msg when category has been deleted through api

--- app/Enum/Api/RestResponses.php
+++ app/Enum/Api/RestResponses.php
@@ -24,6 +24,7 @@ enum RestResponses: string
     case USER_IS_NOT_AN_OWNER = 'User which you have typed is not an owner of this topic';
 
     case NEW_CATEGORY_HAS_BEEN_ADDED = 'Your category has been succesfully added!';
+    case CATEGORY_HAS_BEEN_DELETED = "Category has been succesfully deleted!";
     case ERROR_GET_ALL_CATEGORIES = 'Something went wrong when trying to see all categories, please try one more time, or contact with admin';
     case ERROR_ADD_NEW_CATEGORY = 'Something went wrong when adding new category, please try one more time, or contact with admin';
 }
