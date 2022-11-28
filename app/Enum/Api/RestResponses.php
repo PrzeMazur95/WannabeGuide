0ee7e6f872722810cdd4d08a31a4b203de2da586
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-28 20:06:17 +0100

    Add rest respnse enum msg when smth went wrong with delteing category through api

--- app/Enum/Api/RestResponses.php
+++ app/Enum/Api/RestResponses.php
@@ -28,4 +28,5 @@ enum RestResponses: string
     case CATEGORY_HAS_BEEN_DELETED = "Category has been succesfully deleted!";
     case ERROR_GET_ALL_CATEGORIES = 'Something went wrong when trying to see all categories, please try one more time, or contact with admin';
     case ERROR_ADD_NEW_CATEGORY = 'Something went wrong when adding new category, please try one more time, or contact with admin';
+    case ERROR_DELETE_CATEGORY = 'Something went wrong when trying to delete typed category, please try one more time, or contact with admin';
 }
