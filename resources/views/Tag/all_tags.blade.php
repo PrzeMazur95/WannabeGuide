d9f53d7fdaa53677dbf976e0cb8c446d18c31276
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-06 22:41:43 +0100

    Change to tag page layout

--- resources/views/Tag/all_tags.blade.php
+++ resources/views/Tag/all_tags.blade.php
@@ -4,10 +4,12 @@
             {{ __('Here we have all tags') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="grid grid-rows-3 grid-flow-col gap-4">
-        <div class="row-span-3 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">01</div>
-        <div class="col-span-2 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">02</div>
-        <div class="row-span-2 bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-2 sm:px-6 lg:px-8 p-6 mx-2 ...">03</div>
+    <div class="flex ...">
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">mysql</div>
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">linux</div>
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">windows</div>
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">php</div>
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">laravel</div>
+        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">ci</div>
     </div>
 </x-app-layout>
