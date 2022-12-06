2bc7cae11b2f1457368043695d02cc9653d2ba3f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-06 23:06:52 +0100

    Change to tag page layout

--- resources/views/Tag/all_tags.blade.php
+++ resources/views/Tag/all_tags.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
             {{ __('Here we have all tags') }}
         </h2>
-    </x-slot>
-    <div class="flex ...">
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">mysql</div>
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">linux</div>
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">windows</div>
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">php</div>
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">laravel</div>
-        <div class="flex-1 bg-white sm:rounded-lg mx-2 my-2 text-center h-32 w-32 p-4 ...">ci</div>
-    </div>
+        <div class="flex flex-wrap align-center">
+            @forelse ( $tags as $tag )
+                <div class="bg-white sm:rounded-lg mx-2 my-2 text-center h-24 w-24 p-4 ..."><p class="truncate ...">{{ $tag->name }}</p></div>
+            @empty
+                <div class="bg-white sm:rounded-lg mx-2 my-2 text-center h-24 w-24 p-4 ...">There are no tags yet</div>
+            @endforelse
+        </div>
 </x-app-layout>
