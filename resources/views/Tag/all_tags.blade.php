0406816f0e5005ebee81eb1400559fad4f63b4e4
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-10 21:58:09 +0100

    Add delete tag on all tags page

--- resources/views/Tag/all_tags.blade.php
+++ resources/views/Tag/all_tags.blade.php
@@ -7,9 +7,13 @@
             @forelse ( $tags as $tag )
                 <div class="bg-white sm:rounded-lg mx-2 my-2 text-center h-24 w-24 p-4 ...">
                     <p class="truncate ...">{{ $tag->name }}</p>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold px-1 my-6 rounded">
-                        <p class="text-sm">x</p>
-                    </button>
+                    <form method="POST" action="{{ route('tag.delete', ['tag'=>$tag->id]) }}">
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <x-deleteButton>
+                            {{ __('x') }}
+                        </x-deleteButton>
+                    </form>
                 </div>
             @empty
                 <div class="bg-white sm:rounded-lg mx-2 my-2 text-center h-24 w-24 p-4 ...">There are no tags yet</div>
