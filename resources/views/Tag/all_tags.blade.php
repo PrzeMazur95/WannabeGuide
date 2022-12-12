14a9e1d3224df0cb75f51ea769fa25c30dfaeef0
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-12 20:12:36 +0100

    Add session msg on tags page if one of them has been deleted

--- resources/views/Tag/all_tags.blade.php
+++ resources/views/Tag/all_tags.blade.php
@@ -5,6 +5,11 @@
         </h2>
     </x-slot>
     <x-slot name="slot">
+        @if(Session::has('TAG_DELETED'))
+            <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                <span class="font-medium">{{ Session::get('TAG_DELETED')}}</span>
+            </div>
+        @endif
         <div class="flex flex-wrap align-center">
             @forelse ( $tags as $tag )
                 <div class="bg-white sm:rounded-lg mx-2 my-2 text-center h-24 w-24 p-4 ...">
