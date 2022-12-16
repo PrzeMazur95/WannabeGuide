d1213ac963bb1d4e85bf889c2cb8e12524ea986e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-16 08:45:25 +0100

    Add belongs to many relationship in Topic model to retrieve tags

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\User;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Topic extends Model
 {
@@ -28,5 +30,15 @@ class Topic extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Relation between topic and tag
+     *
+     * @return BelongsToMany
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
