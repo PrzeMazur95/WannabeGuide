d1e3184d38e07b2f7d9fe073abd8f53f951336b7
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-25 11:25:56 +0100

    Create method to attach tags to a given topic

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -6,4 +6,17 @@ namespace App\Services;
 
 class TagService {
 
+    /**
+     * Method attaching tags to a given topic
+     *
+     * @param object $topic
+     * @param array $tags_id
+     * 
+     * @return void
+     */
+    public function attachTagsToTopic($topic, $tags_id): void
+    {
+
+        $topic->tags()->sync($tags_id);
+    }
 }
