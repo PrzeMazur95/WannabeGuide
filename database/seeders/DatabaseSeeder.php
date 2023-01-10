9b6b826e2bfd4f9da7637c5f77f1305f161a0e40
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-01-10 20:07:41 +0100

    Add Tag to seeder, to create also this fake data at the beggining

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
 use App\Models\Topic;
 use App\Models\User;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -28,5 +28,7 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make("password"),
             ]
         );
+        Tag::factory(1)->create();
+        Topic::factory()->has(Tag::factory()->count(2))->create();
     }
 }
