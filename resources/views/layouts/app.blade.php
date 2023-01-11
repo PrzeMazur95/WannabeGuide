a7e684b7daf64dfd3e0d66c8007f695a9ce81b38
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-01-11 22:38:42 +0100

    Add slot for scripts under jquery cdn on every page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,5 +30,6 @@
             </main>
         </div>
         <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+            {{ $script }}
     </body>
 </html>
