00ff76ce063d65f84ffdee9fc9376ea65beeab06
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-01-14 19:55:26 +0100

    add div that will show founded topics

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,6 +18,19 @@
             </div>
         </div>
     </div>
+    <div>
+        <div class="mb-6">
+            <a href="some_url">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div id="test" class="p-6 bg-white border-b border-gray-200">
+                            founded topics
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
     <div class="grid grid-rows-3 grid-flow-col gap-4">
   <div class="row-span-3 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">01</div>
   <div class="col-span-2 bg-white overflow-hidden shadow-sm sm:rounded-lg sm:px-6 lg:px-8 p-6 mx-2 ...">02</div>
