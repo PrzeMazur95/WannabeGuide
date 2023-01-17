ed2454134d565d5faf158ea7bc59d78a984ac895
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-01-17 18:29:38 +0100

    Extend controller, to return dashboard with additional data which will be displayed on dashboard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,51 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Topic;
+use App\Enum\LoggerMessages;
+use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * 
+     */
+    public function __construct(
+        private Category $category, 
+        private Tag $tag, 
+        private Topic $topic,
+        private Log $logger
+    )
+    {
+    }
+
+    /**
+     * Returns dashobard view, with counted topics, tags, categories
+     * 
+     * @return View
+     */
+    public function dashboard($categoriesCount = 0, $tagsCount = 0, $topicsCount = 0): View
+    {
+        try {
+            $categoriesCount = $this->category::all()->count();
+            $tagsCount = $this->tag::all()->count();
+            $topicsCount = $this->topic::all()->count();
+        } catch ( \Exception $e) { 
+            
+            $this->logger::error(LoggerMessages::ERROR_CONNECTION_WITH_DB->value, ['error' => $e->getMessage()]);
+        }
+        
+        return view('dashboard', [
+            'categoriesCount'=>$categoriesCount, 
+            'tagsCount'=>$tagsCount, 
+            'topicsCount'=>$topicsCount
+        ]);
+    }
 }
